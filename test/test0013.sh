5cbcd0eca658fbb9646f1c48f01916c10c3bcbe2
Lorinczy Zsigmond <zsigmond.lorinczy@idomsoft.hu>
2024-10-23 21:16:41 +0200

    test0013: describe with synonyms

---
+++ test/test0013.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Testing synonyms
+
+Perl="${Perl:-$(which perl)}"
+PerlOpt="${PerlOpt:-}"
+
+YaSql="${YaSql:-$(which yasql)}"
+YaSqlOpt="${YaSqlOpt:-}"
+
+export LC_CTYPE=en_US.UTF-8
+export NLS_LANG=american_america.AL32UTF8
+export NLS_NCHAR=AL32UTF8
+
+"$Perl" $PerlOpt "$YaSql" $YaSqlOpt $(cat ~/secret/scott.dbuid) <<DONE
+describe dual
+describe sys.dual
+describe public.dual
+
+describe user_tables
+describe sys.user_tables
+describe public.user_tables
+DONE
